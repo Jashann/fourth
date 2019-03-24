434257256af5b8a3079ba0b826434922bd263260
Jashann <jashangill3592@gmail.com>
2019-03-24 18:41:44 +0530

    weather converted to https

--- Programs/Weather/weather.js
+++ Programs/Weather/weather.js
@@ -7,7 +7,7 @@ class Weather
     }
     async getWeather(city)
     {
-        const http = await fetch(`http://api.apixu.com/v1/current.json?key=5bf73fbf7227481b94941431192403&q=${city}`);
+        const http = await fetch(`https://api.apixu.com/v1/current.json?key=5bf73fbf7227481b94941431192403&q=${city}`);
         const response = await http.json();
         return response;
     }
